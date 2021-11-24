51920e9be4d1aa420014266fbab878b7b3fc8172
rachid-webdev <rasheedolamide2015@gmail.com>
2021-11-24 19:57:57 +0100

    project data updated

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -79,14 +79,14 @@ export const Blogs = [
     imgSrc: 'https://i.ibb.co/WD5mZCP/Screenshot-26.png',
     link: '#!',
   },
+
   {
     id: 10,
-    name: 'Boldlinks Academy Trainee Website on Web Development',
-    tags: ['MySql', 'Wordpress', 'Custom Css'],
-    date: '5 July, 2021',
-    imgSrc:
-      'https://i.ibb.co/0K44mBJ/academy-boldlinks-com-ng-Large-Screen-1280x800.png',
-    link: 'https://academy.boldlinks.com.ng/',
+    name: 'Roatek Technologies New Agency Website Build with Laravel Framework',
+    tags: ['Laravel', 'MySql', 'BladeTemplate', 'PHP', 'Schema'],
+    date: '26 May, 2021',
+    imgSrc: 'https://i.ibb.co/rbmpHFP/roatek-com-ng-Large-Screen-1280x800.png',
+    link: 'https://roatek.com.ng',
   },
   {
     id: 11,
@@ -98,24 +98,16 @@ export const Blogs = [
   },
   {
     id: 12,
-    name: 'How to build a Fabulous Todo App using React, Redux and Framer-Motion',
-    tags: ['react', 'redux', 'framer-motion'],
-    date: '26 May, 2021',
-    imgSrc:
-      'https://res.cloudinary.com/practicaldev/image/fetch/s--r5oUDhhT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/036s7j5b73up7455rdth.png',
-    link: 'https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1',
-  },
-  {
-    id: 13,
-    name: 'What is Higher Order Component (HOC) in React JS?',
-    tags: ['react', 'reactJS', 'web-development'],
+    name: 'Le Rambouillet premier luxury concierge Website Redesign Challenge',
+    tags: ['Bootstrap', 'Jquery', 'Webdevelopment', 'CustomCSS'],
     date: '3 Feb, 2021',
     imgSrc:
-      'https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75',
-    link: 'https://codebucks.hashnode.dev/what-is-higher-order-component',
+      'https://i.ibb.co/hHz7ZGw/rachid-webdev-github-io-Large-Screen-1280x800.png',
+    link: 'https://rachid-webdev.github.io/lerambouillet.com-website-redesign/',
   },
+
   {
-    id: 14,
+    id: 13,
     name: 'Lovely Effect Basics Calculator Design with Html, Css and JavaScript ',
     tags: ['Html', 'JavaScript', 'Css'],
     date: '5 Jan, 2021',
@@ -124,31 +116,21 @@ export const Blogs = [
     link: 'https://roatek-technologies.github.io/calculator/',
   },
   {
-    id: 15,
-    name: 'How to implement Pagination Component in React from scratch',
-    tags: ['react', 'css'],
+    id: 14,
+    name: 'Chat Bot Design Using Monkedev API ',
+    tags: ['Axios', 'CustomCss', 'API', 'FontAwesome', 'JavaScript'],
     date: '5 Jan, 2021',
     imgSrc:
-      'https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75',
-    link: 'https://codebucks.hashnode.dev/pagination-in-react',
-  },
-
-  {
-    id: 16,
-    name: 'What is Redux ? Simply Explained!',
-    tags: ['react', 'redux', 'javascript'],
-    date: '16 May, 2021',
-    imgSrc:
-      'https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png',
-    link: 'https://dev.to/codebucks/what-is-redux-simply-explained-2ch7',
+      'https://i.ibb.co/jk77xzt/roatek-chatbot-netlify-app-Large-Screen-1280x800.png',
+    link: 'https://roatek-chatbot.netlify.app/',
   },
   {
-    id: 17,
-    name: 'How to create Sidebar navigation menu in ReactJS with react router and framer-motion',
-    tags: ['react', 'framer-motion', 'styled-components'],
-    date: '26 May, 2021',
+    id: 15,
+    name: 'Boldlinks Academy Trainee Website on Web Development',
+    tags: ['MySql', 'Wordpress', 'Custom Css'],
+    date: '5 July, 2021',
     imgSrc:
-      'https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png',
-    link: 'https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3',
+      'https://i.ibb.co/0K44mBJ/academy-boldlinks-com-ng-Large-Screen-1280x800.png',
+    link: 'https://academy.boldlinks.com.ng/',
   },
 ];
